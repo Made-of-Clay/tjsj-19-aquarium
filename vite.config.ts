c109ffbcbb6fcb822da75a18a1d39c464fba3a3e
Adam Leis <adam.m.leis@gmail.com>
2025-08-12 21:59:53 -0400

    add vite config for vendor chunking

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+// basic vite config with manual chunk for node_modules (vendor chunk)
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
