495800bf84ce983e86edc980c470e8f2110ebd2a
Adam Leis <adam.m.leis@gmail.com>
2025-08-22 23:11:33 -0400

    remove commented code & update notes

--- NOTES.md
+++ NOTES.md
@@ -1,7 +1,9 @@
 # Notes
 
 - [x] animated fish in bubble
-  - [ ] animate bubble/fish group to move along path
+  - [x] animate bubble/fish group to move along path
+    - I've abandoned this for now - requires a mesh to be animated on the path, and when I tried grabbing the coords from a cube that was moving along the path, I couldn't get real values. I'll hack around this for now.
+  - [x] animate fish along central axis (e.g. change transform origin outside the bubble and rotate around it to look like swimming in a circle)
 - [x] bridge or land mass fish can swim around
   - [x] textures for realism
   - consider more complex example than just a plane (e.g. plane w/ notches cut out, inside of cylinder with parts missing, Blender-modeled fragmented tunnel, etc.)
--- src/getBubble.ts
+++ src/getBubble.ts
@@ -1,46 +1,10 @@
 import GUI from 'lil-gui';
-import { AnimationMixer, BoxGeometry, BufferGeometry, CatmullRomCurve3, Color, CubeTexture, DoubleSide, Float32BufferAttribute, Group, Line, LineBasicMaterial, LoadingManager, Mesh, MeshBasicMaterial, PerspectiveCamera, Points, PointsMaterial, Quaternion, Scene, ShaderMaterial, SphereGeometry, Vector3 } from 'three'
+import { AnimationMixer, Color, CubeTexture, DoubleSide, Group, LoadingManager, Mesh, PerspectiveCamera, Quaternion, Scene, ShaderMaterial, SphereGeometry } from 'three'
 import waterVertexShader from './waterCube.vert?raw'
 import waterFragmentShader from './waterCube.frag?raw';
-import { Flow, GLTFLoader } from 'three/examples/jsm/Addons.js';
-
-// Example (https://threejs.org/examples/?q=modi#webgl_modifier_curve) 
-// and source (https://github.com/mrdoob/three.js/blob/master/examples/webgl_modifier_curve.html#L72)
-// ----
-// init testing path points
-// const pointPos = [
-//     10, 2, -6,
-//     10, 2, 6,
-//     0, 6, 10,
-//     -10, 2, 6,
-//     -10, 2, -6,
-//     0, 6, -10,
-// ];
-// const curvePoints: Vector3[] = [];
-// for (let i = 0; i < pointPos.length; i += 3) {
-//     curvePoints.push(new Vector3(pointPos[i], pointPos[i + 1], pointPos[i + 2]));
-// }
-
-// // TODO refactor (or make new) as class that returns creature in bubble
-
-// const dotGeo = new BufferGeometry();
-// dotGeo.setAttribute('position', new Float32BufferAttribute(pointPos, 3));
-// const dotMat = new PointsMaterial({ color: 0xff00ff });
-// const points = new Points(dotGeo, dotMat);
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
 export function getBubble(gui: GUI, envMap: CubeTexture, scene: Scene, loadingManager: LoadingManager) {
-    // for testing
-    // scene.add(points);
-
-    // draw line from pointPos
-    // const curve = new CatmullRomCurve3(curvePoints, true);
-    // const line = new Line(
-    //     new BufferGeometry().setFromPoints(curve.getPoints(100)),
-    //     new LineBasicMaterial({ color: 0xff0000 }),
-    // );
-    // scene.add(line);
-
-    // real code
     const bubbleGroup = new Group();
     bubbleGroup.name = 'Bubble Group';
 
@@ -88,17 +52,6 @@ export function getBubble(gui: GUI, envMap: CubeTexture, scene: Scene, loadingMa
     bubbleGroup.add(bubble);
     console.log('bubble group', bubbleGroup);
 
-    // animate bubble group along path - NOTICE leaving this here for legacy
-    // might return to path animation, but it's fairly clear I can't do Group
-    // animation along a path; only mesh
-    // const cube = new Mesh(
-    //     new BoxGeometry(3, 3, 3),
-    //     new MeshBasicMaterial({ color: 0x00ff00, wireframe: true }),
-    // );
-    // const cubeFlow = new Flow(cube);
-    // cubeFlow.updateCurve(0, curve);
-    // scene.add(cubeFlow.object3D);
-
     let mixer: AnimationMixer | null = null;
 
     const loader = new GLTFLoader(loadingManager);
@@ -106,7 +59,7 @@ export function getBubble(gui: GUI, envMap: CubeTexture, scene: Scene, loadingMa
     loader.load(
         'models/koi/scene.gltf',
         gltf => {
-            // NOTE imported models shuold be cloned via Object3d.clone() for best efficiency
+            // NOTE imported models should be cloned via Object3d.clone() for best efficiency
             // i.e. fish model can be reused if cloned
             // gltf.scene.clone();
             bubbleGroup.add(gltf.scene);
@@ -122,19 +75,13 @@ export function getBubble(gui: GUI, envMap: CubeTexture, scene: Scene, loadingMa
         console.error,
     );
 
-    let globalPosition = new Vector3();
     // IDEA: use collision detection to increase the wave amplitude during collisions indicating disturbances
     function animateBubble(elapsedTime: number, camera: PerspectiveCamera, deltaTime: number) {
         waterCubeMaterial.uniforms.time.value = elapsedTime;
         waterCubeMaterial.uniforms.cameraQuaternion.value.copy(camera.quaternion)
         const safeDelta = Math.min(Math.max(deltaTime, 1/120), 1/24); // between 1/120s and 1/24s
         mixer?.update(safeDelta * 8);
-        // cubeFlow.moveAlongCurve(0.001);
-        // animatedBubbleGroup.moveAlongCurve(0.001);
-        // globalPosition.setFromMatrixPosition(cubeFlow.)
-        // console.log('--', cubeFlow.getWorldPosition(globalPosition));
-        // console.log('>>', globalPosition);
     }
-    
-    return { animateBubble, bubbleGroup/* , animatedBubbleGroup */ };
+
+    return { animateBubble, bubbleGroup };
 }
